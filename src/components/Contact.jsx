a15fc9e8d4c04341010d5f11c1d333c9df06e94e
Martyna Bogdan <martynabogdan@Martynas-MacBook-Pro.local>
2024-04-25 14:46:39 +0200

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,172 +1,168 @@
-/** @format */
-import { useRef } from "react";
-import emailjs from "@emailjs/browser";
-import { useState, useContext } from "react";
-import Title from "./Title";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTty } from "@fortawesome/free-solid-svg-icons";
-import "../styles/all.scss";
-import {
-  validateName,
-  validateNumber,
-  validateEmail,
-  validateMessage,
-} from "../utilis/validateErrors.js";
-import { Context } from "../context/context";
-import Error from "./Error";
+import React, { useState, useRef, useContext } from 'react'
+import emailjs from '@emailjs/browser'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTty } from '@fortawesome/free-solid-svg-icons'
+import '../styles/all.scss'
+import { validateName, validateNumber, validateEmail, validateMessage } from '../utilis/validateErrors.js'
+import { Context } from '../context/context'
+import Error from './Error'
+import RotateLoader from 'react-spinners/ClipLoader'
+import Title from './Title.jsx'
 
 const Contact = () => {
-  const form = useRef();
-  const defaultFormData = {
-    fname: "",
-    email: "",
-    number: "",
-    message: "",
-  };
+	const form = useRef()
+	const defaultFormData = {
+		fname: '',
+		email: '',
+		number: '',
+		message: '',
+	}
 
-  const [formData, setFormData] = useState(defaultFormData);
-  const { formErrors, setFormErrorsWrapper } = useContext(Context);
-  const [successForm, setSuccessForm] = useState(false);
+	const [formData, setFormData] = useState(defaultFormData)
+	const { formErrors, setFormErrorsWrapper } = useContext(Context)
+	const [successForm, setSuccessForm] = useState(false)
+	const [loading, setLoading] = useState(false)
 
-  const validateInput = (name, value) => {
-    switch (name) {
-      case "fname":
-        validateName(value, setFormErrorsWrapper);
-        break;
-      case "number":
-        validateNumber(value, setFormErrorsWrapper);
-        break;
-      case "email":
-        validateEmail(value, setFormErrorsWrapper);
-        break;
-      case "message":
-        validateMessage(value, setFormErrorsWrapper);
-        break;
-      default:
-        break;
-    }
-  };
+	const validateInput = (name, value) => {
+		switch (name) {
+			case 'fname':
+				validateName(value, setFormErrorsWrapper)
+				break
+			case 'number':
+				validateNumber(value, setFormErrorsWrapper)
+				break
+			case 'email':
+				validateEmail(value, setFormErrorsWrapper)
+				break
+			case 'message':
+				validateMessage(value, setFormErrorsWrapper)
+				break
+			default:
+				break
+		}
+	}
 
-  const handleFormInputChange = (e, name, value) => {
-    e.preventDefault();
-    validateInput(name, value);
-    setFormData((formData) => ({ ...formData, [name]: value }));
-    isValidForm();
-  };
+	const handleFormInputChange = (e, name, value) => {
+		e.preventDefault()
+		validateInput(name, value)
+		setFormData(formData => ({ ...formData, [name]: value }))
+		isValidForm()
+	}
 
-  const isValidForm = () => {
-    if (formErrors) {
-      return Object.values(formErrors).every(
-        (currentValue) => currentValue === ""
-      );
-    }
-  };
+	const isValidForm = () => {
+		if (formErrors) {
+			return Object.values(formErrors).every(currentValue => currentValue === '')
+		}
+	}
 
-  const isEmptyForm = () => {
-    return Object.entries(formData).every(([k, v]) => v !== "");
-  };
+	const isEmptyForm = () => {
+		return Object.entries(formData).every(([k, v]) => v !== '')
+	}
 
-  const sendEmailFun = (e) => {
-    e.preventDefault();
-    emailjs
-      .sendForm(
-        process.env.REACT_APP_EMAILJS_SERVICE_ID,
-        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
-        form.current,
-        {
-          publicKey: process.env.REACT_APP_EMAILJS_USER_ID,
-        }
-      )
-      .then(
-        () => {
-          console.log("SUCCESS!");
-          setSuccessForm(true);
-          setTimeout(() => {
-            setFormData(defaultFormData);
-            setSuccessForm(false);
-          }, 3000);
-        },
-        (error) => {
-          console.log("FAILED...", error.text);
-          setSuccessForm(false);
-        }
-      );
-  };
-  return (
-    <>
-      <Title title="Lets talk"></Title>
-      <div className="form__wrapper" id="contact">
-        <form className="form" ref={form}>
-          <h1 className="form__title">&bull; Keep in Touch &bull;</h1>
-          <div className="form__icon">
-            <FontAwesomeIcon icon={faTty} />
-          </div>
-          <div className="form__row">
-            <div className="form__name">
-              <label forhtml="name"></label>
-              <input
-                type="text"
-                placeholder="My name is"
-                name="fname"
-                id="name_input"
-                value={formData.fname}
-                onChange={(e) => handleFormInputChange(e, e.target.name, e.target.value)}
-              ></input>
-            </div>
-            <div className="form__email">
-              <label forhtml="email"></label>
-              <input
-                type="email"
-                placeholder="My email is"
-                name="email"
-                id="email_input"
-                value={formData.email}
-                onChange={(e) => handleFormInputChange(e, e.target.name, e.target.value)}
-              ></input>
-            </div>
-          </div>
-          <div className="form__number">
-            <label forhtml="number"></label>
-            <input
-              type="text"
-              placeholder="My number is"
-              name="number"
-              id="number_input"
-              value={formData.number}
-              onChange={(e) => handleFormInputChange(e, e.target.name, e.target.value)}
-            ></input>
-          </div>
-          <div className="form__message">
-            <label forhtml="message"></label>
-            <textarea
-              name="message"
-              placeholder="I'd like to chat about"
-              id="message_input"
-              value={formData.message}
-              onChange={(e) => handleFormInputChange(e, e.target.name, e.target.value)}
-              cols="50"
-              rows="5"
-            ></textarea>
-          </div>
-          {formErrors && <Error formErrors={formErrors} />}
-          <div className="form__submit">
-            <button
-              id="form_button"
-              disabled={!isEmptyForm() || !isValidForm()}
-              onClick={(e) => sendEmailFun(e)}
-            >
-              SEND MESSAGE!
-            </button>
-          </div>
-          {successForm ? (
-            <p className="success-btn">
-              THANK YOU! 🖤 I'll get back to you soon!{" "}
-            </p>
-          ) : null}
-        </form>
-      </div>
-    </>
-  );
-};
+	const override = {
+		width: '20px',
+		height: '20px',
+	}
 
-export default Contact;
+	const sendEmailFun = e => {
+		e.preventDefault()
+		setLoading(true)
+		emailjs
+			.sendForm(process.env.REACT_APP_EMAILJS_SERVICE_ID, process.env.REACT_APP_EMAILJS_TEMPLATE_ID, form.current, {
+				publicKey: process.env.REACT_APP_EMAILJS_USER_ID,
+			})
+			.then(
+				() => {
+					console.log('SUCCESS!')
+					setSuccessForm(true)
+					setTimeout(() => {
+						setFormData(defaultFormData)
+						setSuccessForm(false)
+					}, 3000)
+				},
+				error => {
+					console.log('FAILED...', error.text)
+					setSuccessForm(false)
+				}
+			)
+			.finally(() => {
+				setLoading(false) // This will ensure the spinner stops after success or failure
+			})
+	}
+
+	return (
+		<>
+			<Title title='Lets talk'></Title>
+			<div className='form__wrapper' id='contact'>
+				<form className='form' ref={form}>
+					<h1 className='form__title'>&bull; Keep in Touch &bull;</h1>
+					<div className='form__icon'>
+						<FontAwesomeIcon icon={faTty} />
+					</div>
+					<div className='form__row'>
+						<div className='form__name'>
+							<label htmlFor='name'></label>
+							<input
+								type='text'
+								placeholder='My name is'
+								name='fname'
+								id='name_input'
+								value={formData.fname}
+								onChange={e => handleFormInputChange(e, e.target.name, e.target.value)}
+							></input>
+						</div>
+						<div className='form__email'>
+							<label htmlFor='email'></label>
+							<input
+								type='email'
+								placeholder='My email is'
+								name='email'
+								id='email_input'
+								value={formData.email}
+								onChange={e => handleFormInputChange(e, e.target.name, e.target.value)}
+							></input>
+						</div>
+					</div>
+					<div className='form__number'>
+						<label htmlFor='number'></label>
+						<input
+							type='text'
+							placeholder='My number is'
+							name='number'
+							id='number_input'
+							value={formData.number}
+							onChange={e => handleFormInputChange(e, e.target.name, e.target.value)}
+						></input>
+					</div>
+					<div className='form__message'>
+						<label htmlFor='message'></label>
+						<textarea
+							name='message'
+							placeholder="I'd like to chat about"
+							id='message_input'
+							value={formData.message}
+							onChange={e => handleFormInputChange(e, e.target.name, e.target.value)}
+							cols='50'
+							rows='5'
+						></textarea>
+					</div>
+					{formErrors && <Error formErrors={formErrors} />}
+
+					{loading ? (
+						<RotateLoader cssOverride={override} loading={loading} color='#6c5e5b' className='spinner' />
+					) : (
+						<div className='form__submit'>
+							<button id='form_button' disabled={!isEmptyForm() || !isValidForm()} onClick={e => sendEmailFun(e)}>
+								SEND MESSAGE!
+							</button>
+						</div>
+					)}
+
+					{successForm ? <p className='success-btn'>THANK YOU! 🖤 I'll get back to you soon! </p> : null}
+				</form>
+			</div>
+		</>
+	)
+}
+
+export default Contact
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import Title from './Title'
 import '../styles/projects.scss'
-import project1 from '../assets/images/project-1.png'
+import project1 from '../assets/images/project1.png'
 import project2 from '../assets/images/project2.png'
 import project4 from '../assets/images/project4.png'
 import project3 from '../assets/images/project3.png'
@@ -18,8 +18,9 @@ const Projects = () => {
 			<div className='projects__wrapper'>
 				<Project
 					title='App'
-					subtitle='Eat App'
-					description="An application enabling food ordering. It's built using React, TypeScript, MUI, and localStorage, ensuring data persistence even after browser refresh! You can add and remove food items from the cart, search and sort food items, and a form to collect and verify your details. Total price will always be updated."
+					subtitle='Food App'
+					description="An application enabling food ordering. It's built using React, TypeScript & MUI. Ensuring data persistence even after browser refresh! You can add and remove food items from the cart, search and sort food items, and a form to collect and verify your details. Total price will always be updated."
+					technologies='REACT, TYPESCRIPT, REACT ROUTER DOM, MUI & SCSS '
 					hrefLive='https://eat-app-ts.vercel.app/all-food'
 					hrefCode='https://github.com/martynakiljan/eat-app-ts'
 					projectUrl={project5}
@@ -27,7 +28,7 @@ const Projects = () => {
 				<Project
 					title='Website'
 					subtitle='for ZweiM Bau GmbH Company'
-					description='Website for a company specializing in dry construction. The company wanted a simple and minimalist website. A website built with client communication in mind :)'
+					description='Website for a company specializing in dry construction. The company wanted a simple and minimalist website. A Website with built-in customer communication :)'
 					technologies='REACT & SCSS'
 					hrefLive='https://www.zweimbau.ch/'
 					hrefCode='https://github.com/martynakiljan/website-zweiMBau'
@@ -42,15 +43,7 @@ const Projects = () => {
 					hrefCode='https://github.com/martynakiljan/todo-react'
 					projectUrl={project2}
 				/>
-				<Project
-					title='Website'
-					subtitle='For Foos Event Company'
-					description='A website for cool people who are champions in table football :) They wanted a colorful website - and they got it!'
-					technologies='REACT, MUI & SCSS'
-					hrefLive='https://www.foos-events.com/'
-					hrefCode='https://github.com/martynakiljan/foos-football-website'
-					projectUrl={project3}
-				/>
+
 				<Project
 					title='App'
 					subtitle='The New York Times App'
@@ -60,10 +53,19 @@ const Projects = () => {
 					projectUrl={project4}
 				/>
 				<Project
+					title='Website'
+					subtitle='For Foos Event Company'
+					description='A website for cool people who are champions in table football :) They wanted a colorful website - and they got it!'
+					technologies='JAVASCRIPT & CSS'
+					hrefLive='https://www.foos-events.com/'
+					hrefCode='https://github.com/martynakiljan/foos-football-website'
+					projectUrl={project3}
+				/>
+				<Project
 					title='App'
 					subtitle='Book Hotel App (IN PROGRESS)'
 					description='This is an application resembling Booking.com, but much smaller, built with React, TS and Tailwind CSS. It has greater potential and will be expanded :)'
-					technologies='REACT, TYPESCRIPT & TAILWIND CSS'
+					technologies='REACT, TYPESCRIPT, REACT ROUTER DOM & TAILWIND CSS'
 					hrefCode='https://github.com/martynakiljan/book-hotel-app'
 					projectUrl={project6}
 				/>
