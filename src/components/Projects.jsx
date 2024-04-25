d047cd0a7f15ffd6435da7821f586051eb514bb1
Martyna Bogdan <martynabogdan@Martynas-MacBook-Pro.local>
2024-04-25 09:18:39 +0200

    update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,4 @@
 /** @format */
-<<<<<<< HEAD
 import React from 'react'
 import Title from './Title'
 import '../styles/projects.scss'
@@ -73,64 +72,5 @@ const Projects = () => {
 		</>
 	)
 }
-=======
-import Title from "./Title";
-import "../styles/projects.scss";
-import project1 from "../assets/images/project1.png";
-import project2 from "../assets/images/project2.png";
-import project4 from "../assets/images/project4.png";
-import project3 from "../assets/images/project3.png";
-import Project from "./Project";
-import MoreProjectsInfo from "./MoreProjectsInfo";
-
-const Projects = () => {
-  return (
-    <>
-      <Title title="Projects" id="projects" />
-      <div className="projects__wrapper">
-        <Project
-          title="Website"
-          subtitle="for ZweiM Bau GmbH Company"
-          description="Website for a company specializing in dry construction. The
-              company wanted a simple and minimalist website."
-          technologies="REACT, CSS, NPM"
-          hrefLive="https://www.zweimbau.ch/"
-          hrefCode="https://github.com/martynakiljan/website-zweiMBau"
-          projectUrl={project1}
-        />
-        <Project
-          title="App"
-          subtitle="Todo List"
-          description="An application that helps you create tasks. You can add a task,
-          remove it, mark it as important, move it up or down, or you can
-          drag and drop it, you can also mark it as finished!"
-          technologies="REACT, MUI, NPM, CSS"
-          hrefLive="https://todo-react-indol-psi.vercel.app/"
-          hrefCode="https://github.com/martynakiljan/todo-react"
-          projectUrl={project2}
-        />
-        <Project
-          title="Website"
-          subtitle="For Foos Event Company"
-          description="A website for cool people who are champions in table football :)"
-          technologies="REACT, MUI, NPM, CSS"
-          hrefLive="https://www.foos-events.com/"
-          hrefCode="https://github.com/martynakiljan/foos-football-website"
-          projectUrl={project3}
-        />
-        <Project
-          title="App"
-          subtitle="The New York Times App"
-          description="An application that shows news from around the world"
-          technologies="REACT, NPM, CSS, STYLED COMPONENTS, FETCH API"
-          hrefCode="https://github.com/martynakiljan/the-new-york-times-app/tree/main"
-          projectUrl={project4}
-        />
-        <MoreProjectsInfo />
-      </div>
-    </>
-  );
-};
->>>>>>> cbcf44c7b515515e09b211eba324bc1dadc18252
 
 export default Projects
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,7 +3,6 @@ import Title from './Title'
 import '../styles/skills.scss'
 
 const Skills = () => {
-<<<<<<< HEAD
 	return (
 		<>
 			<Title title='Skills' id='skills' />
@@ -33,32 +32,5 @@ const Skills = () => {
 		</>
 	)
 }
-=======
-  return (
-    <>
-      <Title title="Skills" id="skills" />
-      <div className="skills__wrapper skills__wrapper--hard">
-        <p className="skill">HTML</p>
-        <p className="skill">CSS</p>
-        <p className="skill">JAVA SCRIPT</p>
-        <p className="skill">REACT</p>
-        <p className="skill">TYPE SCRIPT</p>
-        <p className="skill">BOOTSTRAP</p>
-        <p className="skill">RWD</p>
-        <p className="skill">NPM</p>
-        <p className="skill">WEBPACK</p>
-        <p className="skill">API</p>
-        <p className="skill">FIGMA/ADOBE INDESIGN</p>
-      </div>
-      <p className="skills__and">&</p>
-      <div className="skills__wrapper skills__wrapper--soft">
-        <p className="skill">I'm ambitious.</p>
-        <p className="skill">I manage my time well.</p>
-        <p className="skill">I'm going to my destination.</p>
-      </div>
-    </>
-  );
-};
->>>>>>> cbcf44c7b515515e09b211eba324bc1dadc18252
 
 export default Skills
